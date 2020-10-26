c82b90a798bdcc8f0563a334635b3a9205b5448e
Dmitry Novikov <novicovdm@gmail.com>
2020-10-26 05:28:50 +0300

    Solved task044

--- src/main/java/com/epam/university/java/core/task044/Task044Impl.java
+++ src/main/java/com/epam/university/java/core/task044/Task044Impl.java
@@ -2,16 +2,7 @@ package com.epam.university.java.core.task044;
 
 import java.util.List;
 
-/**
- * Author Dmitry Novikov 23-Oct-20.
- */
 public class Task044Impl implements Task044 {
-    /**
-     * count of traces in the snow.
-     * @param points total number of points
-     * @param lines  each element is a two points separated by space
-     * @return number of traces
-     */
     @Override
     public int findCountOfTraces(Integer points, List<String> lines) {
         if (points == null || lines == null) {
@@ -22,7 +13,52 @@ public class Task044Impl implements Task044 {
             return points;
         }
 
+        int counter = 0;
+        boolean intersects = true;
 
-        return 0;
+        for (int i = 0; i < lines.size(); i++) {
+
+            if (intersects) {
+                intersects = false;
+            } else {
+                counter++;
+            }
+
+            for (int j = 1; j < lines.size(); j++) {
+                if (Integer.parseInt(String.valueOf(lines.get(i).charAt(2)))
+                        == Integer.parseInt(String.valueOf(lines.get(j).charAt(0)))) {
+                    intersects = true;
+                }
+            }
+        }
+
+        if (!intersects) {
+            counter++;
+        }
+
+        if (findMaxNumber(lines) != points) {
+            counter += points - findMaxNumber(lines);
+        }
+
+        return counter;
+    }
+
+    /**
+     * Method returns the max number in the list of Strings.
+     *
+     * @param list of Strings
+     * @return int max number
+     */
+    public static int findMaxNumber(List<String> list) {
+        int maxNumber = 0;
+        for (String s : list
+        ) {
+            int maxInSet = Math.max(Integer.parseInt(String.valueOf(s.charAt(0))),
+                    Integer.parseInt(String.valueOf(s.charAt(2))));
+            if (maxNumber < maxInSet) {
+                maxNumber = maxInSet;
+            }
+        }
+        return maxNumber;
     }
 }
